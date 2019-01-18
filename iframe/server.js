c7db6912a384d69760a5bc2483d45899508fa084
Zach Ferland <zachferland@gmail.com>
2019-01-18 14:59:51 -0500

    add readme, change ports and configs

--- iframe/server.js
+++ iframe/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
-const port = 30001
+const port = 3031
 
 const app = express()
 app.use(serveStatic(path.join(__dirname), {'index': ['./iframe.html']}))
--- primary/index.js
+++ primary/index.js
@@ -1,9 +1,10 @@
-const IFRAME_STORE_URL = "http://230242fb.ngrok.io"
+// REPLACE IFRAME URL HERE
+const IFRAME_URL = "http://230242fb.ngrok.io"
 
 startButton.addEventListener('click', () => {
   console.log('click!')
   const iframe = document.createElement('iframe')
-  iframe.src = IFRAME_STORE_URL
+  iframe.src = IFRAME_URL
   iframe.style = 'width:0; height:0; border:0; border:none !important'
   document.body.appendChild(iframe)
 })
